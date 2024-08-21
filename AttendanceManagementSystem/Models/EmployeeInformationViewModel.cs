8c6792129ab84fea9cfba371b4f9811cf67a4f8f
kinoshita <k.kinoshita05@gmail.com>
2024-08-21 11:28:47 +0900

    従業員一覧画面作成

---
+++ AttendanceManagementSystem/Models/EmployeeInformationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagementSystem.Models
+{
+    public class EmployeeInformationViewModel
+    {
+        /// <summary>
+        /// 従業員ID
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// 従業員名
+        /// </summary>
+        public string EmployeeName { get; set; } = default!;
+
+        /// <summary>
+        /// 性別
+        /// </summary>
+        public int Gender { get; set; }
+
+        /// <summary>
+        /// 電話番号
+        /// </summary>
+        public string PhoneNumber { get; set; } = default!;
+
+        /// <summary>
+        /// 住所
+        /// </summary>
+        public string Address { get; set; } = default!;
+
+        /// <summary>
+        /// 建物名
+        /// </summary>
+        public string? BuildingName { get; set; }
+
+        /// <summary>
+        /// 入社日
+        /// </summary>
+        public DateTime HireDate { get; set; }
+
+        /// <summary>
+        /// 退社日
+        /// </summary>
+        public DateTime? ResignDate { get; set; }
+
+        /// <summary>
+        /// 更新日時
+        /// </summary>
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
