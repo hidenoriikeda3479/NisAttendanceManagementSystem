062a714ad06c008cd1b14f33b8666ced456f0088
LAPTOP-9R2M4RU8\USER <gfacet0525@gmail.com>
2024-09-03 20:09:09 +0900

    ログイン画面作成

--- AttendanceManagementSystem/Views/Login.Designer.cs
+++ AttendanceManagementSystem/Views/Login.Designer.cs
@@ -29,44 +29,79 @@
         private void InitializeComponent()
         {
             btnLogin = new Button();
-            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            employeenameTextBox1 = new TextBox();
+            passwordTextBox2 = new TextBox();
             SuspendLayout();
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(12, 12);
+            btnLogin.Location = new Point(126, 173);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(75, 23);
             btnLogin.TabIndex = 0;
             btnLogin.Text = "ログイン";
             btnLogin.UseVisualStyleBackColor = true;
-            btnLogin.Click += button1_Click;
+            btnLogin.Click += btnLogin_Click;
             // 
-            // button2
+            // label1
             // 
-            button2.Location = new Point(12, 41);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "Sample";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 14F);
+            label1.Location = new Point(12, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 25);
+            label1.TabIndex = 2;
+            label1.Text = "従業員名";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 14F);
+            label2.Location = new Point(12, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 25);
+            label2.TabIndex = 2;
+            label2.Text = "パスワード";
+            // 
+            // employeenameTextBox1
+            // 
+            employeenameTextBox1.Location = new Point(106, 57);
+            employeenameTextBox1.Name = "employeenameTextBox1";
+            employeenameTextBox1.Size = new Size(221, 23);
+            employeenameTextBox1.TabIndex = 3;
+            // 
+            // passwordTextBox2
+            // 
+            passwordTextBox2.Location = new Point(106, 113);
+            passwordTextBox2.Name = "passwordTextBox2";
+            passwordTextBox2.PasswordChar = '*';
+            passwordTextBox2.Size = new Size(221, 23);
+            passwordTextBox2.TabIndex = 3;
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
+            ClientSize = new Size(336, 208);
+            Controls.Add(passwordTextBox2);
+            Controls.Add(employeenameTextBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(btnLogin);
             Name = "Login";
-            Text = "Form1";
+            Text = "ログイン画面";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnLogin;
-        private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox employeenameTextBox1;
+        private TextBox passwordTextBox2;
     }
 }
--- AttendanceManagementSystem/Views/Login.cs
+++ AttendanceManagementSystem/Views/Login.cs
@@ -1,15 +1,25 @@
+using AttendanceManagementSystem.Common;
 using AttendanceManagementSystem.Data;
 using AttendanceManagementSystem.Views;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace AttendanceManagementSystem
 {
+    /// <summary>
+    /// ログイン画面
+    /// </summary>
     public partial class Login : Form
     {
         /// <summary>
         /// DBコンテキスト
         /// </summary>
         private readonly AttendanceManagementDbContext _context;
+
+        /// <summary>
+        /// ログイン登録した社員ID
+        /// </summary>
+        private int Id;
 
         /// <summary>
         /// コンストラクタ
@@ -21,18 +31,60 @@ namespace AttendanceManagementSystem
             _context = context;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// ログインボタンクリック
+        /// </summary>
+        /// <param name="sender">コントロール情報</param>
+        /// <param name="e">イベント情報</param>
+        private void btnLogin_Click(object sender, EventArgs e)
         {
-            var menu = new Menu(_context);
-            menu.Show();
-            Hide();
+            //入力状態チェック
+            TextInputCheck();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 入力状態チェック
+        /// </summary>
+        private void TextInputCheck()
         {
-            var sample = new Sample(_context);
-            sample.Show();
+
+            //従業員名、パスワード入力チェック
+            if (string.IsNullOrEmpty(employeenameTextBox1.Text) ||
+                string.IsNullOrEmpty(passwordTextBox2.Text))
+            {
+                //ボックス内容に未入力表示
+                MessageBox.Show("必須項目が未入力です");
+                return;
+            }
+
+            //入力されたパスワードのハッシュ化
+            string hash = HashHelper.sha512(passwordTextBox2.Text);
+
+            //従業員名とパスワードを照合、一致する社員を取得
+            var matchrecord = _context.Employees.SingleOrDefault(n => n.EmployeeName == employeenameTextBox1.Text &&
+                                                                 n.Password == hash);
+
+            //入力情報が間違っていた場合
+            if (matchrecord == null)
+            {
+                //ボックス内容にエラー表示
+                MessageBox.Show("従業員名またはパスワードが間違っています");
+                return;
+            }
+            else
+            {
+                //一致した社員のIDを取得
+                Id = matchrecord.EmployeeId;
+            }
+
+            //ログインした社員IDを取得して、メニュー画面へ遷移
+            var menu = new Menu(_context, Id);
+
+            menu.Show();
+
             Hide();
+
         }
+
     }
 }
